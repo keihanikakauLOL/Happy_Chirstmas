7f049f07cb87867000b7dac6b2aeaec4b2ecec55
keihanikakauLOL <arm.nantana@gmail.com>
2025-12-26 15:00:13 +0700

    nice

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,25 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    //random func
+    srand(time(0));
+    int predict = rand() % 9;
+
+    string trash;
+    string grade[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+    cout << "Press Enter 3 times to reveal your future.\n";
+
+    for (int i = 0; i < 3; i++) {
+        getline(cin, trash);
+    }
+
+    cout << "You will get " << grade[predict] << " in this 261102.";
+    
+}
